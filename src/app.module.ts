864a5d0c0f5c6e46ebdf9fc7c273c4649969597b
ivanreis96 <ivreis96@gmail.com>
2024-04-28 19:01:20 -0300

    Inserção de categorias, modulo e controller, e adição da biblioteca de validação do nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [RestaurantModule],
+  imports: [RestaurantModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })

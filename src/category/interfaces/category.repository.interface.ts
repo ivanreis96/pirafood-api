b54f1131db38e7013f02d749bb41e7512e683547
ivanreis96 <ivreis96@gmail.com>
2024-04-29 20:44:10 -0300

    Alteração na interface do repositório de categoria

--- src/category/interfaces/category.repository.interface.ts
+++ src/category/interfaces/category.repository.interface.ts
@@ -4,5 +4,5 @@ import { Category } from "../schemas/category.schema";
 export default interface ICategoryRepository{
     get():Promise<Category>;
     update(data: Category): Promise<void>
+    create(data: Partial<Category>): Promise<Category>
 }
-// sdasdasdas

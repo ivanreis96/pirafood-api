c9846aaa43215b04ad382a8063f3df51433787ec
ivanreis96 <ivreis96@gmail.com>
2024-04-29 20:21:06 -0300

    Adição da branch develop

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { CategoryModule } from './category/category.module';
-import { TestModule } from './test/test.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [RestaurantModule, CategoryModule, TestModule],
+  imports: [RestaurantModule, CategoryModule, MongooseModule.forRoot('mongodb+srv://ivan:ZQQP5rcelzdyQmF5@pirafood.mebzoz2.mongodb.net/?retryWrites=true&w=majority&appName=PiraFood')],
   controllers: [AppController],
   providers: [AppService],
 })

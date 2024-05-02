0fde92068f9c2256d1b53aaf3f4d3022dcb1f890
ivanreis96 <ivreis96@gmail.com>
2024-05-01 22:36:29 -0300

    adição controller update category

--- src/category/dtos/update.category.controller.input.ts
+++ src/category/dtos/update.category.controller.input.ts
@@ -1,7 +1,6 @@
 import { IsNotEmpty, IsString } from "class-validator";
 
-export default class CreateCategoryControllerInput{
-
+export default class UpdateCategoryControllerInput{
     @IsString()
     @IsNotEmpty()
     name:string;
--- src/category/interfaces/category.repository.interface.ts
+++ src/category/interfaces/category.repository.interface.ts
@@ -1,6 +1,4 @@
 import { Category } from "../schemas/category.schema";
-import CreateCategoryUseCaseInput from "../usecases/dtos/create.category.usecase.input";
-
 
 export default interface ICategoryRepository{
     create(data: Partial<Category>): Promise<Category>
